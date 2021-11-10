14daa1db3540c2d5ae55390ff27f3e1a3c07650c
ahsong-sqisoft <ah.song@sqisoft.com>
2021-11-10 16:07:03 +0900

    game view에서 player를 main camera가 따라가기

--- Assets/02.Scripts/FollowCam.cs
+++ Assets/02.Scripts/FollowCam.cs
@@ -2,12 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FllowCam : MonoBehaviour
+public class FollowCam : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // 따라가야 할 대상을 연결할 변수
+    public Transform targetTr;
+    // Main Camera 자신의 Transform 컴포넌트
+    private Transform camTr;
+
+    // 따라갈 대상으로부터 떨어질 거리
+    [Range(2.0f, 20.0f)] //[Ranget(min, max)] 어트리뷰트를 사용하면 다음라인에 선언한 변수의 입력 번위를 제한하여 인스펙터 뷰에 슬라이드바를 표시
+    public float distance = 10.0f;
+
+    
+    // Y축으로 이동할 높이
+    [Range(0.0f, 10.0f)]
+    public float height = 2.0f;
+
     void Start()
     {
-        
+        // Main Camera 자신의 Transform 컴포넌트를 추출
+        camTr = GetComponent<Transform>();
+    }
+
+    void LateUpdate()
+    {
+        // 추적해야 할 대상의 뒤쪽으로 distance만큼 이동
+        // 높이를 height만큼 이동
+        camTr.position = targetTr.position + (-targetTr.forward * distance) + (Vector3.up * height);
+
+        // Camera를 피벗 좌표를 향해 회전
+        camTr.LookAt(targetTr.position);
     }
 
     // Update is called once per frame
