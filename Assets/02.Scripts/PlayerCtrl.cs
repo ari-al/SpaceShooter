dc54f82d59748d3f16763befed0589cf94cf3a9a
ahsong-sqisoft <ah.song@sqisoft.com>
2021-11-10 11:47:49 +0900

    mouse 입력 rotate 조정

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -8,6 +8,8 @@ public class PlayerCtrl : MonoBehaviour
     private Transform tr;
     // 이동 속도 변수 (public으로 선언되어 인스펙터 뷰에 노출됨)
     public float moveSpeed = 10.0f;
+    // 회전 속도 변수
+    public float turnSpeed = 80.0f;
     
     // Start is called before the first frame update
     void Start()
@@ -21,6 +23,7 @@ public class PlayerCtrl : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        float r = Input.GetAxis("Mouse X");
 
         Debug.Log("h=" + h);
         Debug.Log("v=" + v);
@@ -39,6 +42,14 @@ public class PlayerCtrl : MonoBehaviour
         // 매 초 10유닛씩 이동
         //tr.Translate(Vector3.forward * Time.deltaTime * 10);
 
-        tr.Translate(Vector3.forward * Time.deltaTime * v * moveSpeed);
+       // tr.Translate(Vector3.forward * Time.deltaTime * v * moveSpeed);
+
+       // 전후좌우 이동 방향 벡터 계산
+       Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+       // Translate(이동 방향 * 속력 * Time.deltaTime) normalized는 정규화 벡터값을 사용하도록 한다. -> 대각선 이동도 길이가 1인 벡터로 변환
+        tr.Translate(moveDir.normalized * Time.deltaTime * moveSpeed);
+
+       // Vector3.up 축을 기준으로 turnSpeed만큼의 속도로 회전 
+        tr.Rotate(Vector3.up * turnSpeed * Time.deltaTime * r);
     }
 }
