56d8e77226366423f3e13c51dddcee14d4efd980
ahsong-sqisoft <ah.song@sqisoft.com>
2021-11-10 16:24:36 +0900

    카메라 이동 구면 선형 보간, SmoothDamp 적용

--- Assets/02.Scripts/FollowCam.cs
+++ Assets/02.Scripts/FollowCam.cs
@@ -18,6 +18,12 @@ public class FollowCam : MonoBehaviour
     [Range(0.0f, 10.0f)]
     public float height = 2.0f;
 
+    // 반응 속도
+    public float damping = 10.0f;
+
+    // SmoothDamp에 사용할 변수
+    private Vector3 velocity = Vector3.zero;
+
     void Start()
     {
         // Main Camera 자신의 Transform 컴포넌트를 추출
@@ -28,7 +34,19 @@ public class FollowCam : MonoBehaviour
     {
         // 추적해야 할 대상의 뒤쪽으로 distance만큼 이동
         // 높이를 height만큼 이동
-        camTr.position = targetTr.position + (-targetTr.forward * distance) + (Vector3.up * height);
+        // camTr.position = targetTr.position + (-targetTr.forward * distance) + (Vector3.up * height);
+        Vector3 pos = targetTr.position + (-targetTr.forward * distance) + (Vector3.up * height);
+       
+        // 구면 선형 보간 함수를 사용해 부드럽게 위치를 변경
+        // camTr.position = Vector3.Slerp(camTr.position,              // 시작 위치
+        //                                 pos,                        // 목표 위치
+        //                                 Time.deltaTime * damping);  // 시간 t
+
+        // SmoothDamp을 이용한 위치 보간
+        camTr.position = Vector3.SmoothDamp(camTr.position,         // 시작 위치
+                                            pos,                    // 목표 위치
+                                            ref velocity,           // 현재 속도
+                                            damping);               // 목표 위치까지 도달하 시간
 
         // Camera를 피벗 좌표를 향해 회전
         camTr.LookAt(targetTr.position);
