9ab460ef5112371eab4164eff9f8259f370f5d06
ahsong-sqisoft <ah.song@sqisoft.com>
2021-11-10 11:28:43 +0900

    detaTime 적용해 up,down 키 입력

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    // 컴포넌트를 캐시 처리할 함수
+    private Transform tr;
+    // 이동 속도 변수 (public으로 선언되어 인스펙터 뷰에 노출됨)
+    public float moveSpeed = 10.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Transform 컴포넌트를 추출해 변수에 대입
+        tr = GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -20,6 +26,19 @@ public class PlayerCtrl : MonoBehaviour
         Debug.Log("v=" + v);
 
         // Transform 컴포넌트의 position 속성값을 변경
-        transform.position += new Vector3(0, 0, 1);
+        // transform.position += new Vector3(0, 0, 1);
+
+        
+        // 정규화 벡터를 사용한 코드
+        // tr.position += Vector3.forward * 1;
+
+        // Translate 함수르 사용한 이동 로직
+
+        // 프레임마다 10유닛씩 이동 ex) 30fps 300유닛 이동
+        // tr.Translate(Vector3.forward * 10);
+        // 매 초 10유닛씩 이동
+        //tr.Translate(Vector3.forward * Time.deltaTime * 10);
+
+        tr.Translate(Vector3.forward * Time.deltaTime * v * moveSpeed);
     }
 }
